81a1a653e33ac51dffea03bf48c4450932ceca74
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2020-03-02 12:36:43 -0800

    register DI'd wrapper in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import { setObservableWrapper } from './wrapWithDI';
+import { wrapAsOnDemandObservable } from './wrapAsOnDemandObservable';
+setObservableWrapper(wrapAsOnDemandObservable);
+
 export { OnDemandObservableObject } from './OnDemandObservableObject';
 export { OnDemandObservableMap } from './OnDemandObservableMap';
 export { wrapAsOnDemandObservable as observableOnDemand } from './wrapAsOnDemandObservable';
