99af068ee6da3bcddbf6257b31014aade7245b79
Max Huang-Hobbs <mahuangh@microsoft.com>
2020-03-04 09:48:06 -0800

    Separate builds for prod and dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,56 @@
+// @ts-check
 import rollupTypescriptPlugin from 'rollup-plugin-typescript2';
+import rollupReplacePlugin from 'rollup-plugin-replace';
+import { terser as rollupTerserPlugin } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
-export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: pkg.main,
+function getConfig({ isProd }) {
+    const stub = isProd ? 'prod' : 'dev';
+    return {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: `dist/cjs/index.${stub}.js`,
+                format: 'cjs',
+            },
+            {
+                file: `dist/es/index.${stub}.js`,
+                format: 'es',
+            },
+        ],
+        external: [
+            ...Object.keys(pkg.dependencies || {}),
+            ...Object.keys(pkg.peerDependencies || {}),
+        ],
+        plugins: [
+            rollupTypescriptPlugin({
+                typescript: require('typescript'),
+                useTsconfigDeclarationDir: true,
+            }),
+            isProd &&
+                rollupReplacePlugin({
+                    'process.env.NODE_ENV': JSON.stringify('production'),
+                }),
+            isProd && rollupTerserPlugin(),
+        ],
+    };
+}
+
+export default [
+    // Entrypoints for js and cjs
+    {
+        input: 'src/cjs-common-entrypoint.js',
+        output: {
+            file: 'dist/index.js',
             format: 'cjs',
         },
-        {
-            file: pkg.module,
-            format: 'es',
-        },
-    ],
-    external: [
-        ...Object.keys(pkg.dependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {}),
-    ],
-
-    plugins: [
-        rollupTypescriptPlugin({
-            typescript: require('typescript'),
-        }),
-    ],
-};
+        external: ['./index.prod.js', './index.dev.js'],
+    },
+    // Actual build
+    getConfig({
+        isProd: false,
+    }),
+    getConfig({
+        isProd: true,
+    }),
+];
