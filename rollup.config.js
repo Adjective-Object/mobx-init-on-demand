fffa6f4728ff3b8ba04f9006c129ce5c66dc28c8
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2020-03-05 11:05:50 -0800

    build index.dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import rollupTypescriptPlugin from 'rollup-plugin-typescript2';
 import rollupReplacePlugin from 'rollup-plugin-replace';
+import rollupCopyPlugin from 'rollup-plugin-copy';
 import { terser as rollupTerserPlugin } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
@@ -45,7 +46,7 @@ function getConfig({ isProd }) {
 }
 
 export default [
-    // Entrypoints for js and cjs
+    // Entrypoint cjs
     {
         input: 'src/cjs-common-entrypoint.js',
         output: {
@@ -53,6 +54,16 @@ export default [
             format: 'cjs',
         },
         external: ['./index.prod.js', './index.dev.js'],
+        plugins: [
+            rollupCopyPlugin({
+                targets: [
+                    {
+                        src: 'src/index.d.ts',
+                        dest: 'dist',
+                    },
+                ],
+            }),
+        ],
     },
     // Actual build
     getConfig({
